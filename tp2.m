df3ae2c3e7f2fdf5d631cd90174abe9fb20106bd
Nicolas Carolo <nico.carolo@hotmail.com>
2016-06-12 18:48:38 -0300

    Corregido error en legendas de graficos. Falta Nystrom.

--- tp2.m
+++ tp2.m
@@ -27,7 +27,7 @@ soluciones = zeros(11,2,6,5);
 figure(1,'Name', 'v(t) vs t','Position',[50,50,1600,750]);
 figure(2,'Name', 'v´(t) vs t','Position',[50,50,1600,750]);
 figure(3,'Name', 'v(t) vs v´(t)','Position',[50,50,1600,750]);
-epsylon = 1;
+%epsylon = 1;
 for epsylon=1:5
   
   [T UEuler] = Euler(f,a,b,u0,h,epsylon);
@@ -36,7 +36,6 @@ for epsylon=1:5
   soluciones(:,:,2,epsylon) = YRK2;
   [T YRK4]=rk4(f,a,b,u0,h,epsylon);
   soluciones(:,:,3,epsylon) = YRK4;
-  % Metodos de paso fijo
   
   %falta ver la funcion a usar en nystrom
   [T YNys ENys]=nystrom('f_u',a,b,u0,h);
@@ -48,26 +47,29 @@ for epsylon=1:5
   [t,u] = ode45(ode,0:0.1:1,u0,options);
   soluciones(:,:,6,epsylon) = u;
   
+% Grafico de v(t) vs t
   figure(1)
   subplot (2, 3, epsylon);
   plot(T,soluciones(:,1,1,epsylon),'c',T,soluciones(:,1,2,epsylon),'r',T,soluciones(:,1,3,epsylon),'g'
   ,T,soluciones(:,1,4,epsylon),'m',T,soluciones(:,1,5,epsylon),'-bo',T,soluciones(:,1,6,epsylon),'k');
   title(strcat('Epsylon = ' , num2str(epsylon)));
-  xlabel('v(t)');
-  ylabel('t');
+  xlabel('t');
+  ylabel('v(t)');
   legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','west');
   legend('boxoff');
   
+ % Grafico de v´(t) vs t
   figure(2)
   subplot (2, 3, epsylon);
   plot(T,soluciones(:,2,1,epsylon),'c',T,soluciones(:,2,2,epsylon),'r',T,soluciones(:,2,3,epsylon),'g'
   ,T,soluciones(:,2,4,epsylon),'m',T,soluciones(:,2,5,epsylon),'-bo',T,soluciones(:,2,6,epsylon),'k');
   title(strcat('Epsylon = ' , num2str(epsylon)));
-  xlabel('v´(t)');
-  ylabel('t');
+  xlabel('t');
+  ylabel('v´(t)');
   legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','west');
   legend('boxoff');
   
+ % Grafico de v´(t) vs v(t)
   figure(3)
   subplot (2, 3, epsylon);
   plot(soluciones(:,1,1,epsylon),soluciones(:,2,1,epsylon),'c',soluciones(:,1,2,epsylon),soluciones(:,2,2,epsylon),'r',soluciones(:,1,3,epsylon),soluciones(:,2,3,epsylon),'g'
@@ -78,7 +80,6 @@ for epsylon=1:5
   legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','west');
   legend('boxoff');
 end
-
 % printf (resultado con z = %d\n, z)
 %hold
 %plot(T,UEuler,'r');
