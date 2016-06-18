7a00f346fe7dba04bbd0550f46db02c339808385
Nicolas Carolo <nico.carolo@hotmail.com>
2016-06-17 23:49:28 -0300

    Agregado Graficos 1 por cada uno. Y carpeta de graficos con h= 0.01

--- tp2.m
+++ tp2.m
@@ -21,13 +21,12 @@ options = odeset;
 %     [Nystrom => 4]
 %     [ODE23 => 5]
 %     [ODE25 => 6]
-soluciones = zeros(3001,2,6,5);
+soluciones = zeros(((b-a)/h)+1,2,6,5);
 
 figure(1,'Name', 'v(t) vs t','Position',[50,50,1600,750]);
 figure(2,'Name', 'v´(t) vs t','Position',[50,50,1600,750]);
 figure(3,'Name', 'v(t) vs v´(t)','Position',[50,50,1600,750]);
-%epsylon = 1;
-for epsylon=1:1
+for epsylon=1:5
   
   [T UEuler] = Euler(f,a,b,u0,h,epsylon);
   soluciones(:,:,1,epsylon) = UEuler;
@@ -36,25 +35,61 @@ for epsylon=1:1
   [T YRK4]=rk4(f,a,b,u0,h,epsylon);
   soluciones(:,:,3,epsylon) = YRK4;
   
-%  T'
-  %falta ver la funcion a usar en nystrom
   [T YNys]=nyst(f,a,b,u0,h,epsylon);
-%  T'
-%  YRK2'
-%  YNys'
   
   soluciones(:,:,4,epsylon) = YNys;
   ode = @(t,u) uprima(t,u,epsylon);
-  [t,u] = ode23(ode,a:h:b,u0,options);
-  soluciones(:,:,5,epsylon) = u;
-  [t,u] = ode45(ode,a:h:b,u0,options);
-  soluciones(:,:,6,epsylon) = u;
+  [t23,u23] = ode23(ode,[a b],u0,options);
+  if (epsylon == 1)
+      u23con1 = u23;
+      t23con1 = t23;
+  endif
+  if (epsylon == 2)
+      u23con2 = u23;
+      t23con2 = t23;
+  endif
+  if (epsylon == 3)
+      u23con3 = u23;
+      t23con3 = t23;
+  endif
+  if (epsylon == 4)
+      u23con4 = u23;
+      t23con4 = t23;
+  endif
+  if (epsylon == 5)
+      u23con5 = u23;
+      t23con5 = t23;
+  endif
+%  t23'
+%  soluciones(:,:,5,epsylon) = u;
+  [t45,u45] = ode45(ode,[a b],u0,options);
+  if (epsylon == 1)
+      u45con1 = u45;
+      t45con1 = t45;
+  endif
+  if (epsylon == 2)
+      u45con2 = u45;
+      t45con2 = t45;
+  endif
+  if (epsylon == 3)
+      u45con3 = u45;
+      t45con3 = t45;
+  endif
+  if (epsylon == 4)
+      u45con4 = u45;
+      t45con4 = t45;
+  endif
+  if (epsylon == 5)
+      u45con5 = u45;
+      t45con5 = t45;
+  endif
+%  soluciones(:,:,6,epsylon) = u;
   
 % Grafico de v(t) vs t
   figure(1)
   subplot (3, 2, epsylon);
   plot(T,soluciones(:,1,1,epsylon),'c',T,soluciones(:,1,2,epsylon),'r',T,soluciones(:,1,3,epsylon),'g'
-  ,T,soluciones(:,1,4,epsylon),'m',T,soluciones(:,1,5,epsylon),'-b',T,soluciones(:,1,6,epsylon),'k');
+  ,T,soluciones(:,1,4,epsylon),'m',t23,u23(:,1),'-.b',t45,u45(:,1),'-k');
   title(strcat('Epsylon = ' , num2str(epsylon)));
   xlabel('t');
   ylabel('v(t)');
@@ -65,7 +100,7 @@ for epsylon=1:1
   figure(2)
   subplot (3, 2, epsylon);
   plot(T,soluciones(:,2,1,epsylon),'c',T,soluciones(:,2,2,epsylon),'r',T,soluciones(:,2,3,epsylon),'g'
-  ,T,soluciones(:,2,4,epsylon),'m',T,soluciones(:,2,5,epsylon),'-b',T,soluciones(:,2,6,epsylon),'k');
+  ,T,soluciones(:,2,4,epsylon),'m',t23,u23(:,2),'-.b',t45,u45(:,2),'-k');
   title(strcat('Epsylon = ' , num2str(epsylon)));
   xlabel('t');
   ylabel('v´(t)');
@@ -76,31 +111,161 @@ for epsylon=1:1
   figure(3)
   subplot (3, 2, epsylon);
   plot(soluciones(:,1,1,epsylon),soluciones(:,2,1,epsylon),'c',soluciones(:,1,2,epsylon),soluciones(:,2,2,epsylon),'r',soluciones(:,1,3,epsylon),soluciones(:,2,3,epsylon),'g'
-  ,soluciones(:,1,4,epsylon),soluciones(:,2,4,epsylon),'m',soluciones(:,1,5,epsylon),soluciones(:,2,5,epsylon),'-b',soluciones(:,1,6,epsylon),soluciones(:,2,6,epsylon),'k');
+  ,soluciones(:,1,4,epsylon),soluciones(:,2,4,epsylon),'m',u23(:,1),u23(:,2),'-.b',u45(:,1),u45(:,2),'-k');
   title(strcat('Epsylon = ' , num2str(epsylon)));
   xlabel('v(t)');
   ylabel('v´(t)');
   legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
   legend('boxoff');
 end
-% printf (resultado con z = %d\n, z)
-%hold
-%plot(T,UEuler,'r');
-%hold
-%plot(T,YRK2,'c');
-%
-%printf ("resultado con Euler\n")
-%soluciones(:,1,1,1) %Euler con epsylon 1
-%printf ("resultado con RK2\n")
-%soluciones(:,:,2,1) %RK2 con epsylon 1
-%printf ("resultado con RK4\n)"
-%soluciones(:,:,3,1) %RK4 con epsylon 1
-%printf ("resultado con Nystrom\n")
-%soluciones(:,:,4,1) %Nystrom con epsylon 1
-%printf ("resultado con ODE23\n")
-%soluciones(:,:,5,1) %ODE23 con epsylon 1
-%printf ("resultado con ODE45\n")
-%soluciones(:,:,6,1) %ODE45 con epsylon 1
 
-
-
+epsylon = 1;
+figure(4,'Name', 'v(t) vs t','Position',[50,50,1600,750]);
+plot(T,soluciones(:,1,1,epsylon),'c',T,soluciones(:,1,2,epsylon),'r',T,soluciones(:,1,3,epsylon),'g'
+,T,soluciones(:,1,4,epsylon),'m',t23con1,u23con1(:,1),'-.b',t45con1,u45con1(:,1),'-k');
+title(strcat('Epsylon = ' , num2str(epsylon)));
+xlabel('t');
+ylabel('v(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vt1.jpg
+epsylon = 2;
+figure(5,'Name', 'v(t) vs t','Position',[50,50,1600,750]);
+plot(T,soluciones(:,1,1,epsylon),'c',T,soluciones(:,1,2,epsylon),'r',T,soluciones(:,1,3,epsylon),'g'
+,T,soluciones(:,1,4,epsylon),'m',t23con2,u23con2(:,1),'-.b',t45con2,u45con2(:,1),'-k');
+title(strcat('Epsylon = ' , num2str(epsylon)));
+xlabel('t');
+ylabel('v(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vt2.jpg
+epsylon = 3;
+figure(6,'Name', 'v(t) vs t','Position',[50,50,1600,750]);
+plot(T,soluciones(:,1,1,epsylon),'c',T,soluciones(:,1,2,epsylon),'r',T,soluciones(:,1,3,epsylon),'g'
+,T,soluciones(:,1,4,epsylon),'m',t23con3,u23con3(:,1),'-.b',t45con3,u45con3(:,1),'-k');
+title(strcat('Epsylon = ' , num2str(epsylon)));
+xlabel('t');
+ylabel('v(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vt3.jpg
+epsylon = 4;
+figure(7,'Name', 'v(t) vs t','Position',[50,50,1600,750]);
+plot(T,soluciones(:,1,1,epsylon),'c',T,soluciones(:,1,2,epsylon),'r',T,soluciones(:,1,3,epsylon),'g'
+,T,soluciones(:,1,4,epsylon),'m',t23con4,u23con4(:,1),'-.b',t45con4,u45con4(:,1),'-k');
+title('Epsylon = 4');
+xlabel('t');
+ylabel('v(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vt4.jpg
+epsylon = 5;
+figure(8,'Name', 'v(t) vs t','Position',[50,50,1600,750]);
+plot(T,soluciones(:,1,1,epsylon),'c',T,soluciones(:,1,2,epsylon),'r',T,soluciones(:,1,3,epsylon),'g'
+,T,soluciones(:,1,4,epsylon),'m',t23con5,u23con5(:,1),'-.b',t45con5,u45con5(:,1),'-k');
+title(strcat('Epsylon = ' , num2str(epsylon)));
+xlabel('t');
+ylabel('v(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vt5.jpg
+epsylon = 1;
+figure(9,'Name', "v'(t) vs t",'Position',[50,50,1600,750]);
+plot(T,soluciones(:,2,1,epsylon),'c',T,soluciones(:,2,2,epsylon),'r',T,soluciones(:,2,3,epsylon),'g'
+,T,soluciones(:,2,4,epsylon),'m',t23con1,u23con1(:,2),'-.b',t45con1,u45con1(:,2),'-k');
+title(strcat('Epsylon = ' , num2str(epsylon)));
+xlabel('t');
+ylabel('v´(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vpt1.jpg
+epsylon = 2;
+figure(10,'Name', "v'(t) vs t",'Position',[50,50,1600,750]);
+plot(T,soluciones(:,2,1,epsylon),'c',T,soluciones(:,2,2,epsylon),'r',T,soluciones(:,2,3,epsylon),'g'
+,T,soluciones(:,2,4,epsylon),'m',t23con2,u23con2(:,2),'-.b',t45con2,u45con2(:,2),'-k');
+title(strcat('Epsylon = ' , num2str(epsylon)));
+xlabel('t');
+ylabel('v´(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vpt2.jpg
+epsylon = 3;
+figure(11,'Name', "v'(t) vs t",'Position',[50,50,1600,750]);
+plot(T,soluciones(:,2,1,epsylon),'c',T,soluciones(:,2,2,epsylon),'r',T,soluciones(:,2,3,epsylon),'g'
+,T,soluciones(:,2,4,epsylon),'m',t23con3,u23con3(:,2),'-.b',t45con3,u45con3(:,2),'-k');
+title(strcat('Epsylon = ' , num2str(epsylon)));
+xlabel('t');
+ylabel('v´(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vpt3.jpg
+epsylon = 4;
+figure(12,'Name', "v'(t) vs t",'Position',[50,50,1600,750]);
+plot(T,soluciones(:,2,1,epsylon),'c',T,soluciones(:,2,2,epsylon),'r',T,soluciones(:,2,3,epsylon),'g'
+,T,soluciones(:,2,4,epsylon),'m',t23con4,u23con4(:,2),'-.b',t45con4,u45con4(:,2),'-k');
+title('Epsylon = 4');
+xlabel('t');
+ylabel('v´(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vpt4.jpg
+epsylon = 5;
+figure(13,'Name', "v'(t) vs t",'Position',[50,50,1600,750]);
+plot(T,soluciones(:,2,1,epsylon),'c',T,soluciones(:,2,2,epsylon),'r',T,soluciones(:,2,3,epsylon),'g'
+,T,soluciones(:,2,4,epsylon),'m',t23con5,u23con5(:,2),'-.b',t45con5,u45con5(:,2),'-k');
+title('Epsylon = 5');
+xlabel('t');
+ylabel('v´(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vpt5.jpg
+epsylon = 1;
+figure(14,'Name', "v(t) vs v'(t)",'Position',[50,50,1600,750]);
+plot(soluciones(:,1,1,epsylon),soluciones(:,2,1,epsylon),'c',soluciones(:,1,2,epsylon),soluciones(:,2,2,epsylon),'r',soluciones(:,1,3,epsylon),soluciones(:,2,3,epsylon),'g'
+,soluciones(:,1,4,epsylon),soluciones(:,2,4,epsylon),'m',u23con1(:,1),u23con1(:,2),'-.b',u45con1(:,1),u45con1(:,2),'-k');
+title(strcat('Epsylon = ' , num2str(epsylon)));
+xlabel('v(t)');
+ylabel('v´(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vvp1.jpg
+epsylon = 2;
+figure(15,'Name', "v(t) vs v'(t)",'Position',[50,50,1600,750]);
+plot(soluciones(:,1,1,epsylon),soluciones(:,2,1,epsylon),'c',soluciones(:,1,2,epsylon),soluciones(:,2,2,epsylon),'r',soluciones(:,1,3,epsylon),soluciones(:,2,3,epsylon),'g'
+,soluciones(:,1,4,epsylon),soluciones(:,2,4,epsylon),'m',u23con2(:,1),u23con2(:,2),'-.b',u45con2(:,1),u45con2(:,2),'-k');
+title('Epsylon = 2');
+xlabel('v(t)');
+ylabel('v´(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vvp2.jpg
+epsylon = 3;
+figure(16,'Name', "v(t) vs v'(t)",'Position',[50,50,1600,750]);
+plot(soluciones(:,1,1,epsylon),soluciones(:,2,1,epsylon),'c',soluciones(:,1,2,epsylon),soluciones(:,2,2,epsylon),'r',soluciones(:,1,3,epsylon),soluciones(:,2,3,epsylon),'g'
+,soluciones(:,1,4,epsylon),soluciones(:,2,4,epsylon),'m',u23con3(:,1),u23con3(:,2),'-.b',u45con3(:,1),u45con3(:,2),'-k');
+title(strcat('Epsylon = ' , num2str(epsylon)));
+xlabel('v(t)');
+ylabel('v´(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vvp3.jpg
+epsylon = 4;
+figure(17,'Name', "v(t) vs v'(t)",'Position',[50,50,1600,750]);
+plot(soluciones(:,1,1,epsylon),soluciones(:,2,1,epsylon),'c',soluciones(:,1,2,epsylon),soluciones(:,2,2,epsylon),'r',soluciones(:,1,3,epsylon),soluciones(:,2,3,epsylon),'g'
+,soluciones(:,1,4,epsylon),soluciones(:,2,4,epsylon),'m',u23con4(:,1),u23con4(:,2),'-.b',u45con4(:,1),u45con4(:,2),'-k');
+title(strcat('Epsylon = ' , num2str(epsylon)));
+xlabel('v(t)');
+ylabel('v´(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vvp4.jpg
+epsylon = 5;
+figure(18,'Name', "v(t) vs v'(t)",'Position',[50,50,1600,750]);
+plot(soluciones(:,1,1,epsylon),soluciones(:,2,1,epsylon),'c',soluciones(:,1,2,epsylon),soluciones(:,2,2,epsylon),'r',soluciones(:,1,3,epsylon),soluciones(:,2,3,epsylon),'g'
+,soluciones(:,1,4,epsylon),soluciones(:,2,4,epsylon),'m',u23con5(:,1),u23con5(:,2),'-.b',u45con5(:,1),u45con5(:,2),'-k');
+title(strcat('Epsylon = ' , num2str(epsylon)));
+xlabel('v(t)');
+ylabel('v´(t)');
+legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
+legend('boxoff');
+%print -djpg vvp5.jpg
