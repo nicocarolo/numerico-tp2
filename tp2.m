7271bcfd081686083317e7dc7babfdfd1d1d6135
Nicolas Carolo <nico.carolo@hotmail.com>
2016-06-20 21:47:33 -0300

    Limpio codigo, para entregar tpbis.

--- tp2.m
+++ tp2.m
@@ -1,3 +1,4 @@
+
 %      v    v'
 %    | u0  v0 |
 % U =| u1  v1 |
@@ -28,8 +29,8 @@ figure(2,'Name', 'v´(t) vs t','Position',[50,50,1600,750]);
 figure(3,'Name', 'v(t) vs v´(t)','Position',[50,50,1600,750]);
 for epsylon=1:5
   
-  [T UEuler] = Euler(f,a,b,u0,h,epsylon);
-  soluciones(:,:,1,epsylon) = UEuler;
+  [TEuler UEuler] = Euler(f,a,b,u0,0.0001,epsylon);
+%  soluciones(:,:,1,epsylon) = UEuler;
   [T YRK2 ERK2]=rk2(f,a,b,u0,h,epsylon);
   soluciones(:,:,2,epsylon) = YRK2;
   [T YRK4]=rk4(f,a,b,u0,h,epsylon);
@@ -88,8 +89,8 @@ for epsylon=1:5
 % Grafico de v(t) vs t
   figure(1)
   subplot (3, 2, epsylon);
-  plot(T,soluciones(:,1,1,epsylon),'c',T,soluciones(:,1,2,epsylon),'r',T,soluciones(:,1,3,epsylon),'g'
-  ,T,soluciones(:,1,4,epsylon),'m',t23,u23(:,1),'-.b',t45,u45(:,1),'-k');
+  plot(TEuler,UEuler(:,1),'y',T,soluciones(:,1,2,epsylon),'m--',T,soluciones(:,1,3,epsylon),':c'
+  ,T,soluciones(:,1,4,epsylon),'r',t23,u23(:,1),'-g',t45,u45(:,1),'-b');
   title(strcat('Epsylon = ' , num2str(epsylon)));
   xlabel('t');
   ylabel('v(t)');
@@ -99,8 +100,8 @@ for epsylon=1:5
  % Grafico de v´(t) vs t
   figure(2)
   subplot (3, 2, epsylon);
-  plot(T,soluciones(:,2,1,epsylon),'c',T,soluciones(:,2,2,epsylon),'r',T,soluciones(:,2,3,epsylon),'g'
-  ,T,soluciones(:,2,4,epsylon),'m',t23,u23(:,2),'-.b',t45,u45(:,2),'-k');
+  plot(TEuler,UEuler(:,2),'y',T,soluciones(:,2,2,epsylon),'m--',T,soluciones(:,2,3,epsylon),':c'
+  ,T,soluciones(:,2,4,epsylon),'r',t23,u23(:,2),'-g',t45,u45(:,2),'-b');
   title(strcat('Epsylon = ' , num2str(epsylon)));
   xlabel('t');
   ylabel('v´(t)');
@@ -110,11 +111,18 @@ for epsylon=1:5
  % Grafico de v´(t) vs v(t)
   figure(3)
   subplot (3, 2, epsylon);
-  plot(soluciones(:,1,1,epsylon),soluciones(:,2,1,epsylon),'c',soluciones(:,1,2,epsylon),soluciones(:,2,2,epsylon),'r',soluciones(:,1,3,epsylon),soluciones(:,2,3,epsylon),'g'
-  ,soluciones(:,1,4,epsylon),soluciones(:,2,4,epsylon),'m',u23(:,1),u23(:,2),'-.b',u45(:,1),u45(:,2),'-k');
+  plot(UEuler(:,1),UEuler(:,2),'y',soluciones(:,1,2,epsylon),soluciones(:,2,2,epsylon),'m--',soluciones(:,1,3,epsylon),soluciones(:,2,3,epsylon),':c'
+  ,soluciones(:,1,4,epsylon),soluciones(:,2,4,epsylon),'r',u23(:,1),u23(:,2),'-g',u45(:,1),u45(:,2),'-b');
   title(strcat('Epsylon = ' , num2str(epsylon)));
   xlabel('v(t)');
   ylabel('v´(t)');
   legend('Euler','RK2','RK4','Nystrom','ODE23','ODE45','Location','eastoutside');
   legend('boxoff');
 end
+
+figure(1)
+print("TODOSvt.jpg", "-djpg")
+figure(2)
+print("TODOSv´t.jpg", "-djpg")
+figure(3)
+print("TODOSvv´.jpg", "-djpg")
